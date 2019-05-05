7092e349d6bd8664efaab63b580089fc4e47ca9c
yintao <824050253@qq.com>
2019-05-05 22:17:21 +0800

    第一次提交，开启mvc

--- src/main/java/com/hdu/education/entity/pojo/Result.java
+++ src/main/java/com/hdu/education/entity/pojo/Result.java
@@ -1,5 +1,62 @@
 package com.hdu.education.entity.pojo;
 
-public class Result
+
+public class Result<T>
 {
+    private String status;
+    private T data;
+    private String msg;
+
+    public Result(String status, String msg)
+    {
+        this.status = status;
+        this.msg = msg;
+    }
+
+    public Result(String status, T data, String msg)
+    {
+        this.status = status;
+        this.data = data;
+        this.msg = msg;
+    }
+
+    public String getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(String status)
+    {
+        this.status = status;
+    }
+
+    public T getData()
+    {
+        return data;
+    }
+
+    public void setData(T data)
+    {
+        this.data = data;
+    }
+
+    public String getMsg()
+    {
+        return msg;
+    }
+
+    public void setMsg(String msg)
+    {
+        this.msg = msg;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Result{" +
+                "status='" + status + '\'' +
+                ", data=" + data +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
 }
